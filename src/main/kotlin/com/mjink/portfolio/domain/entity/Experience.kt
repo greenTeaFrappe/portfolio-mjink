5ded8317b184cf4752a16c7b03a0a3cfaadc32b7
greenTeaFrappe <jk008532@gmail.com>
2024-09-30 20:39:14 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/mjink/portfolio/domain/entity/Experience.kt
+++ src/main/kotlin/com/mjink/portfolio/domain/entity/Experience.kt
@@ -3,9 +3,74 @@ package com.mjink.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Experience : BaseEntity() {
+class Experience(
+    title: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    // Exprience -> ExperienceDetail과 1:n 관계
+    // Exprience 하나에 여러 개의 ExperienceDetail 가져올 수 있음
+    // mutableListOf() 빈 리스트, 데이터를 삽입 삭제 관리 가능
+    // FetchType.EGER -> XXX 연관된거 전부 가져옴 (연좌제) LAZY가 더 효율적
+    @OneToMany(
+        targetEntity = ExperienceDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL]
+    ) // 엔티티에서 영속성 컨테스트에 변화가 있었을 때 그 자식 엔티티도 함께 변화 적용
+    @JoinColumn(name = "experience_id")
+    var details: MutableList<ExperienceDetail> = mutableListOf()
+
+    fun getEndYearMonth(): String {
+        if (endYear == null || endMonth == null) {
+            return "Present"
+        }
+        return "${endYear}.${endMonth}"
+    }
+
+    fun update(
+        title: String,
+        description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean
+    ) {
+        this.title = title
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(details: MutableList<ExperienceDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
--- src/main/kotlin/com/mjink/portfolio/domain/entity/Project.kt
+++ src/main/kotlin/com/mjink/portfolio/domain/entity/Project.kt
@@ -3,9 +3,74 @@ package com.mjink.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Project : BaseEntity() {
+class Project(
+    name: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id")
     var id: Long? = null
+
+    var name: String = name
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(
+        targetEntity = ProjectDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL]
+    ) // 엔티티에서 영속성 컨테스트에 변화가 있었을 때 그 자식 엔티티도 함께 변화 적용
+    @JoinColumn(name = "project_id")
+    var details: MutableList<ProjectDetail> = mutableListOf()
+
+    // mappedBy -> 연관관계의 주인 지정 projectSkill이 주인
+    @OneToMany(mappedBy = "project")
+    var skills: MutableList<ProjectSkill> = mutableListOf()
+
+    fun getEndYearMonth(): String {
+        if (endYear == null || endMonth == null) {
+            return "Present"
+        }
+        return "${endYear}.${endMonth}"
+    }
+
+    fun update(
+        name: String,
+        description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean
+    ) {
+        this.name = name
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(details: MutableList<ProjectDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
